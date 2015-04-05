68d011ed764f44a7789828e68bfa8c89968dd25d
nafi11@mhs.if.its.ac.id <Sush1erz.sashimierz>
2015-04-05 20:12:47 +0700

    initial CI

---
+++ application/logs/log-2015-04-05.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2015-04-05 15:08:37 --> Config Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Hooks Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Utf8 Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> UTF-8 Support Enabled
+DEBUG - 2015-04-05 15:08:37 --> URI Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Router Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> No URI present. Default controller set.
+DEBUG - 2015-04-05 15:08:37 --> Output Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Security Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Input Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-05 15:08:37 --> Language Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Loader Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> Controller Class Initialized
+DEBUG - 2015-04-05 15:08:37 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2015-04-05 15:08:37 --> Final output sent to browser
+DEBUG - 2015-04-05 15:08:37 --> Total execution time: 0.1190
